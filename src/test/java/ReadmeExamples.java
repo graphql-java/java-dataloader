25047ce2a7285370a76687cad9bab4cb3e9f51e3
Brad Baker <bbaker@atlassian.com>
2017-09-09 07:31:50 -0700

    Removing graphql dependency from dataloader

--- src/test/java/ReadmeExamples.java
+++ src/test/java/ReadmeExamples.java
@@ -1,16 +1,10 @@
-import graphql.GraphQL;
-import graphql.schema.DataFetcher;
-import graphql.schema.DataFetchingEnvironment;
-import graphql.schema.GraphQLSchema;
 import org.dataloader.BatchLoader;
 import org.dataloader.CacheMap;
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderOptions;
-import org.dataloader.DataLoaderRegistry;
 import org.dataloader.Try;
 import org.dataloader.fixtures.User;
 import org.dataloader.fixtures.UserManager;
-import org.dataloader.graphql.DataLoaderDispatcherInstrumentation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -80,82 +74,6 @@ public class ReadmeExamples {
         userLoader.dispatchAndJoin();
     }
 
-
-    class StarWarsCharacter {
-        List<String> getFriendIds() {
-            return null;
-        }
-    }
-
-    void starWarsExample() {
-
-        // a batch loader function that will be called with N or more keys for batch loading
-        BatchLoader<String, Object> characterBatchLoader = new BatchLoader<String, Object>() {
-            @Override
-            public CompletionStage<List<Object>> load(List<String> keys) {
-                //
-                // we use supplyAsync() of values here for maximum parellisation
-                //
-                return CompletableFuture.supplyAsync(() -> getCharacterDataViaBatchHTTPApi(keys));
-            }
-        };
-
-        // a data loader for characters that points to the character batch loader
-        DataLoader characterDataLoader = new DataLoader<String, Object>(characterBatchLoader);
-
-        //
-        // use this data loader in the data fetchers associated with characters and put them into
-        // the graphql schema (not shown)
-        //
-        DataFetcher heroDataFetcher = new DataFetcher() {
-            @Override
-            public Object get(DataFetchingEnvironment environment) {
-                return characterDataLoader.load("2001"); // R2D2
-            }
-        };
-
-        DataFetcher friendsDataFetcher = new DataFetcher() {
-            @Override
-            public Object get(DataFetchingEnvironment environment) {
-                StarWarsCharacter starWarsCharacter = environment.getSource();
-                List<String> friendIds = starWarsCharacter.getFriendIds();
-                return characterDataLoader.loadMany(friendIds);
-            }
-        };
-
-        //
-        // DataLoaderRegistry is a place to register all data loaders in that needs to be dispatched together
-        // in this case there is 1 but you can have many
-        //
-        DataLoaderRegistry registry = new DataLoaderRegistry();
-        registry.register("character", characterDataLoader);
-
-        //
-        // this instrumentation implementation will dispatched all the dataloaders
-        // as each level fo the graphql query is executed and hence make batched objects
-        // available to the query and the associated DataFetchers
-        //
-        DataLoaderDispatcherInstrumentation dispatcherInstrumentation
-                = new DataLoaderDispatcherInstrumentation(registry);
-
-        //
-        // now build your graphql object and execute queries on it.
-        // the data loader will be invoked via the data fetchers on the
-        // schema fields
-        //
-        GraphQL graphQL = GraphQL.newGraphQL(buildSchema())
-                .instrumentation(dispatcherInstrumentation)
-                .build();
-
-    }
-
-    private GraphQLSchema buildSchema() {
-        return null;
-    }
-
-    private List<Object> getCharacterDataViaBatchHTTPApi(List<String> keys) {
-        return null;
-    }
 
 
     private void tryExample() {
