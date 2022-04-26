e9287320abd6c852eabb0311d8984a18804983f3
samvazquez <samvazquez@expedia.com>
2022-04-26 09:28:14 -0700

    feat: avoid importing all

--- src/main/java/org/dataloader/DataLoaderHelper.java
+++ src/main/java/org/dataloader/DataLoaderHelper.java
@@ -7,7 +7,14 @@ import org.dataloader.stats.StatisticsCollector;
 
 import java.time.Clock;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
