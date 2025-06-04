7a6749e913e5ef7bcf281e15bc99d03a113b11b4
bbaker <bbaker@atlassian.com>
2025-06-04 15:27:18 +1000

    Error Prone / NullAway support for JSpecify - added Kotlin - tweak to assert

--- src/test/kotlin/org/dataloader/KotlinExamples.kt
+++ src/test/kotlin/org/dataloader/KotlinExamples.kt
@@ -2,6 +2,7 @@ package org.dataloader
 
 import org.junit.jupiter.api.Test
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletableFuture.*
 
 /**
  * Some Kotlin code to prove that are JSpecify annotations work here
@@ -12,28 +13,28 @@ class KotlinExamples {
 
     @Test
     fun `basic kotlin test of non nullable value types`() {
-        val dataLoader: DataLoader<String, String> = DataLoaderFactory.newDataLoader { keys -> CompletableFuture.completedFuture(keys.toList()) }
+        val dataLoader: DataLoader<String, String> = DataLoaderFactory.newDataLoader { keys -> completedFuture(keys.toList()) }
 
         val cfA = dataLoader.load("A")
         val cfB = dataLoader.load("B")
 
         dataLoader.dispatch()
 
-        cfA.join().equals("A")
-        cfB.join().equals("B")
+        assert(cfA.join().equals("A"))
+        assert(cfA.join().equals("A"))
     }
 
     @Test
     fun `basic kotlin test of nullable value types`() {
-        val dataLoader: DataLoader<String, String?> = DataLoaderFactory.newDataLoader { keys -> CompletableFuture.completedFuture(keys.toList()) }
+        val dataLoader: DataLoader<String, String?> = DataLoaderFactory.newDataLoader { keys -> completedFuture(keys.toList()) }
 
         val cfA = dataLoader.load("A")
         val cfB = dataLoader.load("B")
 
         dataLoader.dispatch()
 
-        cfA.join().equals("A")
-        cfB.join().equals("B")
+        assert(cfA.join().equals("A"))
+        assert(cfA.join().equals("A"))
     }
 
 }
